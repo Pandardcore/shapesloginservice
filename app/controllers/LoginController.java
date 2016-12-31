84d582566d087421e5d47574f297051fa0f59be1
Lbraka <lbraka@ippon.fr>
2016-12-31 02:42:45 +0100

    Add method for treating incoming login requests

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -17,8 +21,25 @@ public class LoginController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        
-        return ok("Hello. You're logged");
+        return ok("This is the login service of Shapes");
+    }
+
+    public Result login() {
+        if(request().body().asJson() != null) {
+            JsonNode loginNode = request().body().asJson().findValue("login");
+            if(loginNode != null) {
+                return logUser(loginNode.asText());
+            } else {
+                return badRequest("Login parameter missing");
+            }
+        } else {
+            return badRequest("Not a JSON format");
+        }
+    }
+
+    private Result logUser(String login) {
+        System.out.println("Sending login " + login + " to queue");
+        return ok("Hello " + login + ". You've been logged !");
     }
 
 }
